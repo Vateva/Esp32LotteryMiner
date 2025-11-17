c047a7cc8a1f8161a3110a7027b3299e726f90ee
Vateva <153960364+Vateva@users.noreply.github.com>
2025-11-17 03:39:49 +0100

    implemented handle_touch() method in wallet_config_screen

--- src/display/wallet_config_screen.cpp
+++ src/display/wallet_config_screen.cpp
@@ -59,25 +59,136 @@ void WalletConfigScreen::handle_touch(uint16_t tx, uint16_t ty, lgfx::LGFX_Devic
       for (int i = 0; i < TOTAL_SLOTS; i++) {
         uint16_t item_y = LIST_START_Y + (ITEM_HEIGHT * i);
         if (is_point_in_rect(tx, ty, LIST_START_X, item_y, SCREEN_WIDTH, ITEM_HEIGHT)) {
+          selected_item_index = i;
           current_state = STATE_POPUP_MENU;
         }
+      }
+      if (is_point_in_rect(tx, ty, BACK_BUTTON_X, BACK_BUTTON_Y, BACK_BUTTON_W, BACK_BUTTON_HEIGHT)) {
+        // HERE WE WILL GET OUT OF THE WALLETCONFIG INTERFACE
       }
       break;
 
     case STATE_ENTERING_NAME:
-      // handle keyboard input for name
+      kb.handle_touch(tx, ty, lcd);
+
+      if (kb.is_cancelled()) {
+        // user pressed back/cancel, return to list
+        display_needs_redraw = true;
+        current_state = STATE_LIST;
+        kb.clear();  // clears buffer and cancelled flag
+      } else if (kb.is_complete()) {
+        const char* wallet_name = kb.get_text();
+        strncpy(temp_name, wallet_name, MAX_WALLET_NAME_LENGTH);
+        temp_name[MAX_WALLET_NAME_LENGTH] = '\0';
+
+        current_state = STATE_ENTERING_ADDRESS;
+        kb.clear();
+        kb.set_label("Enter wallet address");
+        kb.set_max_length(MAX_WALLET_ADDRESS_LENGTH);
+        kb.reset_complete();
+      }
       break;
 
     case STATE_ENTERING_ADDRESS:
-      // handle keyboard input for address
+      kb.handle_touch(tx, ty, lcd);
+
+      if (kb.is_cancelled()) {
+        // user pressed back/cancel, return to entering name
+        display_needs_redraw = true;
+        current_state = STATE_ENTERING_NAME;
+        kb.clear();  // clears buffer and cancelled flag
+      } else if (kb.is_complete()) {
+        const char* wallet_address = kb.get_text();
+        strncpy(temp_address, wallet_address, MAX_WALLET_ADDRESS_LENGTH);
+        temp_address[MAX_WALLET_ADDRESS_LENGTH] = '\0';
+
+        strcpy(wallets[selected_item_index].name, temp_name);
+        strcpy(wallets[selected_item_index].address, temp_address);
+        wallets[selected_item_index].is_configured = true;
+
+        save_to_nvs(selected_item_index);
+        display_needs_redraw = true;
+        current_state = STATE_LIST;
+        kb.reset_complete();
+      }
       break;
 
     case STATE_POPUP_MENU:
-      // handle popup menu button touches
+      if (wallets[selected_item_index].is_configured) {
+        for (int i = 0; i < 4; i++) {
+          // detect touch select/edit/delete/back buttons
+          if (is_point_in_rect(tx,
+                               ty,
+                               POPUP_MENU_BUTTON_X + (i * POPUP_MENU_BUTTON_WIDTH) + (i * POPUP_MENU_BUTTON_GAP),
+                               POPUP_MENU_BUTTON_Y,
+                               POPUP_MENU_BUTTON_WIDTH,
+                               POPUP_MENU_BUTTON_HEIGHT)) {
+            switch (i) {
+              case 0:
+                for (int j = 0; j < 4; j++) {
+                  wallets[j].is_active = false;
+                }
+
+                wallets[selected_item_index].is_active = true;
+                current_state = STATE_LIST;
+                display_needs_redraw = true;
+                break;
+              case 1:
+                current_state = STATE_ENTERING_NAME;
+
+                kb.clear();
+                kb.set_label("Enter wallet name");
+                kb.set_max_length(MAX_WALLET_ADDRESS_LENGTH);
+                kb.reset_complete();
+                break;
+              case 2:
+                wallets[selected_item_index].name[0] = '\0';
+                wallets[selected_item_index].address[0] = '\0';
+                wallets[selected_item_index].is_active = false;
+                wallets[selected_item_index].is_configured = false;
+                current_state = STATE_LIST;
+                display_needs_redraw = true;
+                break;
+              case 3:
+                current_state = STATE_LIST;
+                display_needs_redraw = true;
+                break;
+              default:
+                break;
+            }
+          }
+        }
+
+      } else {
+        // detect touch in add/cancel buttons
+        for (int i = 1; i < 3; i++) {  // buttons centered in positions 1-2
+          if (is_point_in_rect(tx,
+                               ty,
+                               POPUP_MENU_BUTTON_X + (i * POPUP_MENU_BUTTON_WIDTH) + (i * POPUP_MENU_BUTTON_GAP),
+                               POPUP_MENU_BUTTON_Y,
+                               POPUP_MENU_BUTTON_WIDTH,
+                               POPUP_MENU_BUTTON_HEIGHT)) {
+            switch (i) {
+              case 1:
+                current_state = STATE_ENTERING_NAME;
+
+                kb.clear();
+                kb.set_label("Enter wallet name");
+                kb.set_max_length(MAX_WALLET_ADDRESS_LENGTH);
+                kb.reset_complete();
+                break;
+              case 2:
+                current_state = STATE_LIST;
+                break;
+              default:
+                break;
+            }
+          }
+        }
+      }
       break;
   }
 }
-
 // private drawing methods
 void WalletConfigScreen::draw_header(lgfx::LGFX_Device* lcd) {
   // back button
@@ -118,6 +229,8 @@ void WalletConfigScreen::draw_list_item(uint8_t index, uint16_t x, uint16_t y, l
 
   if (!wallets[index].is_configured) {
     // empty slot "+   Add wallet"
+    lcd->setTextColor(COLOR_WHITE);
+    lcd->setTextSize(2);
     lcd->setCursor(LIST_START_X + 3, y + 12);
     lcd->print("+   Add wallet");
   } else {
@@ -148,12 +261,15 @@ void WalletConfigScreen::draw_list_item(uint8_t index, uint16_t x, uint16_t y, l
 
     // null terminate
     truncated[15] = '\0';
+
+    lcd->print(truncated);
   }
 
   // if active wallet, maybe draw a star or highlight
   if (wallets[index].is_active) {
     lcd->setCursor(SCREEN_WIDTH - (8 * LIST_START_X), y + 12);
-    lcd->print("<-");
+    lcd->setTextSize(2);
+    lcd->print("✓");
   }
 }
 
@@ -167,17 +283,17 @@ void WalletConfigScreen::draw_popup_menu(lgfx::LGFX_Device* lcd) {
   lcd->setTextColor(COLOR_WHITE);
   lcd->setColor(COLOR_WHITE);
   lcd->setTextSize(1);
-  
+
   if (wallets[selected_item_index].is_configured) {
     // draw select/edit/delete/back buttons
     const char* button_labels[] = {"Select", "Edit", "Delete", "Back"};
-    
+
     for (int i = 0; i < 4; i++) {
       lcd->drawRect(POPUP_MENU_BUTTON_X + (i * POPUP_MENU_BUTTON_WIDTH) + (i * POPUP_MENU_BUTTON_GAP),
                     POPUP_MENU_BUTTON_Y,
                     POPUP_MENU_BUTTON_WIDTH,
                     POPUP_MENU_BUTTON_HEIGHT);
-      
+
       lcd->setCursor(POPUP_MENU_BUTTON_TEXT_X + (i * POPUP_MENU_BUTTON_WIDTH) + (i * POPUP_MENU_BUTTON_GAP),
                      POPUP_MENU_BUTTON_TEXT_Y);
       lcd->print(button_labels[i]);
@@ -185,15 +301,14 @@ void WalletConfigScreen::draw_popup_menu(lgfx::LGFX_Device* lcd) {
   } else {
     // draw add/back buttons centered
     const char* button_labels[] = {"Add", "Back"};
-    
-    for (int i = 1; i < 3; i++) {//buttons centered in positions 1-2
 
-      
+    for (int i = 1; i < 3; i++) {  // buttons centered in positions 1-2
+
       lcd->drawRect(POPUP_MENU_BUTTON_X + (i * POPUP_MENU_BUTTON_WIDTH) + (i * POPUP_MENU_BUTTON_GAP),
                     POPUP_MENU_BUTTON_Y,
                     POPUP_MENU_BUTTON_WIDTH,
                     POPUP_MENU_BUTTON_HEIGHT);
-      
+
       lcd->setCursor(POPUP_MENU_BUTTON_TEXT_X + (i * POPUP_MENU_BUTTON_WIDTH) + (i * POPUP_MENU_BUTTON_GAP),
                      POPUP_MENU_BUTTON_TEXT_Y);
       lcd->print(button_labels[i]);
